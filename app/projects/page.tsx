a98ac7ae2f09d08e9768c1dc5db9c1de9552cb39
Eric Gu <tianchenggu@outlook.com>
2025-10-20 20:41:05 -0700

    fix project image aspect ratios

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -166,7 +166,7 @@ const pastProjects: Project[] = [
     description:
       "Designed for Science Olympiad's Robot Tour event. It runs on an Arduino-style RP2040 board with cheap ultrasonic sensors, an IMU, and wheel encoders for localization and dead reckoning. I briefly experimented with optical flow sensors as well. The 3D printed chassis was designed with three goals in mind: easy access to all electronics, compact size to avoid hitting walls, and modularity to easily iterate on the hardware. It won 1st at the 2024 COD Regionals and 4th place at the ISO State competition.",
   image: "/charlotte.JPG",
-    aspectRatio: "5 / 3",
+    aspectRatio: "4 / 3",
     tags: ["C++", "Arduino", "Autodesk Inventor", "Breadboarding", "3D Printing"],
   },
   {
