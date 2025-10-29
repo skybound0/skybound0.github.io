190ef352856a701b71a26d492e449a05f9a37876
Eric Gu <tianchenggu@outlook.com>
2025-10-28 21:28:43 -0700

    fix typos in activities

--- app/page.tsx
+++ app/page.tsx
@@ -169,7 +169,7 @@ const activities: Activity[] = [
         Designing and manufacturing a variety of structures and components using 3D printing, machining, waterjet, and more
       </>),
       (<>
-        Creating curriculum for and teaching a onboarding program with lectures and workshops on CAD, material science, FEA, and more
+        Creating curriculum for and teaching an onboarding program with lectures and workshops on CAD, material science, FEA, and more
       </>),
       (<>
         Producing content for and managing social media channels for outreach and recruitment
@@ -182,7 +182,7 @@ const activities: Activity[] = [
     date: "May 2025 - Aug 2025",
     accomplishments: [
       "Designed the mechanical systems for three generations of autonomous sailboats",
-      "Integrated sailboat control boards, sensors, and actuators with with a unified, easy-to-access mounting system and electrical wiring",
+      "Integrated sailboat control boards, sensors, and actuators with an unified, easy-to-access mounting system",
       "Developed a 0.25m diameter vertical axis wind turbine to power endurance sailboats",
       "Worked through every part of the vehicles' development process, including manufacturing, software deployment, and testing"
     ]
